6c7688feeaa895147dc816486f82dd97808fdc7c
Ali <Alitech3@users.noreply.github.com>
2018-05-08 13:05:21 -0400

    gabe

--- plugins/tutorial.py
+++ plugins/tutorial.py
@@ -4,4 +4,24 @@ from disco.bot import Plugin
 class TutorialPlugin(Plugin):
     @Plugin.command('ping')
     def command_ping(self, event):
-        event.msg.reply('Pong!')
+        event.msg.reply('pong')
+
+    @Plugin.command('test')
+    def command_test(self, event):
+        event.msg.reply('I pass')
+
+    @Plugin.command('add', '<a:int> <b:int>', group='math')
+    def on_math_add_command(self, event, a, b):
+        # Commands can be grouped together for a cleaner user-facing interface.
+        event.msg.reply('It is ' + '{}'.format(a + b))
+
+    @Plugin.command('i wish to file a complaint against', ' <content:str...>')
+    @Plugin.command('im', ' <content:str...>')
+    def on_im_command(self, event, content):
+        event.msg.reply(content + '... you\'re fired.')
+
+    @Plugin.command('echo' '<content:str...>')
+    def on_echo_command(self, event, content):
+        event.msg.reply(content)
+
+    #'{}'.
